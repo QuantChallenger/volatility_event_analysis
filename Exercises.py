664f86ae76cbeeaaeeca6a8424f6f20a138d5c02
QuantChallenger <158506283+QuantChallenger@users.noreply.github.com>
2024-02-15 23:01:45 +0100

    Update Exercises.py

--- Exercises.py
+++ Exercises.py
@@ -1,15 +1,13 @@
 # list
-fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli', '']
-accepted = 'mnopqrstuvwxyz'
-fellowship2 = []
 def filter_names(*arg):
-  
+  fellowship2 = []
     for name in fellowship:
         if any(name.startswith(x) for x in accepted):
             fellowship2.append(name)
     return fellowship2
 
-
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli', '']
+accepted = 'mnopqrstuvwxyz'
 
 result = filter_names(fellowship, accepted)
 print(result)
