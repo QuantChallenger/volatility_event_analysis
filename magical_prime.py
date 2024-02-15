328b3b1037e8a54f578d6be23b17ffac32c168a1
Jean-Sébastien Goupil <contact@jsgoupil.com>
2024-02-15 14:17:37 -0800

    Magical Prime.

---
+++ magical_prime.py
@@ -0,0 +1,5 @@
+# Write a function that will return the index of the first magical prime from an list.
+# A magical prime is something we consider that the addition of two inputs from the array equals a magical prime present in the array.
+# For instance, [1, 2, 15, 17, 38, 42], 2 + 15 = 17, so 17 would be our magical prime.
+# In this case the function should return 3 because 17 is at the 3rd position.
+# In the case you can't find a magical prime, return -1.
