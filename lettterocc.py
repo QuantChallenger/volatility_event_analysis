94c17192f3470c25808227a88d2149f3bee6b20a
QuantChallenger <vincentreynaud98@gmail.com>
2024-02-17 21:47:43 +0100

    Create lettterocc.py

---
+++ lettterocc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 21:37:08 2024
+
+@author: reyna
+"""
+
+"""
+je te donne une string "Bonjour, comment allez vous mon AMIE???"
+et un chiffre: 3
+
+retourne moi une liste de type class LetterOccurence, où tu trouves 3 ou + la lettre même lettre qui se répète
+La case ne devrait pas être prise en compte pour la recherche, les symboles ne comptent pas
+
+Donc ici, je m'attends à recevoir 
+
+o 5 
+n 3
+m 4
+e 3
+
+"""
