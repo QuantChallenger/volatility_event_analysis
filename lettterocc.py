64a9baa0ee3a1f28b5256ba4a68344217fa566e1
QuantChallenger <vincentreynaud98@gmail.com>
2024-04-17 16:11:44 +0200

    Update lettterocc.py

--- lettterocc.py
+++ lettterocc.py
@@ -19,4 +19,75 @@ n 3
 m 4
 e 3
 
+retourner une liste d'instance de classe'
+
 """
+
+"""
+Edge Cases
+
+Toute lettre moins de 3 n'est pas compté
+Les espaces ne peuvent être comptés
+
+
+"""
+"""
+Pseudo code
+
+string list :("Bonjour,...")
+def letter_occurence(letter):
+dict= {}
+boucle 1: pour letter dans string list:
+        keys = dict.keys()
+
+    boucle 2 : pour letter dans string list:
+        dict[letter] += 1
+         if dict[letter]<3
+         pop.keys[letter]
+         
+         return letter_occurence.count
+    
+"""
+
+def letter_occurence(letter):
+    string_list = "Bonjour, comment allez vous mon AMIE???" 
+    dictio = {}
+    for letter in string_list:
+        
+        dictio[letter] += 1
+        
+       
+    
+    return letter_occurence(letter)
+    print("no")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+result= letter_occurence(["e"])
+# 3
+print(result)
+
+
+result= letter_occurence(["m"])
+# 4
+print(result)
+
+result= letter_occurence(["n"])
+# 3
+print(result)
+
+result= letter_occurence(["o"])
+# 5
+print(result)
+
+
+
+
